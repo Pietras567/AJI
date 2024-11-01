f68accb0433267830e960b69bff23ab61cf1fb4e
Łukasz <247660@edu.p.lodz.pl>
2024-11-01 18:44:02 +0100

    podpunkt 3 zrobiony, dodano filtrowanie przy uzyciu daty - checkbox

--- Zadanie 2/scripts.js
+++ Zadanie 2/scripts.js
@@ -1,9 +1,9 @@
 "use strict"
 let todoList = []; //declares a new array for Your todo list
 
-let isTableView = true;
+let dateFilterEnabled = false;
 
-let initList = function() {
+let initList = function () {
     /*
     let savedList = window.localStorage.getItem("todos");
     if (savedList != null)
@@ -27,7 +27,7 @@ let initList = function() {
             // of course the lecture test mentioned above will not take place
         );
     */
-    
+
     //let savedList = window.localStorage.getItem("todos");
     //if (savedList != null)
     //    todoList = JSON.parse(savedList);
@@ -40,7 +40,7 @@ let initList = function() {
                 console.log(req.responseText);
                 let jsonResponse = JSON.parse(req.responseText);
                 console.log(jsonResponse);
-        
+
                 // Sprawdzanie struktury odpowiedzi
                 if (jsonResponse.record) {
                     console.log("jsonResponse.record:", jsonResponse.record);
@@ -52,18 +52,18 @@ let initList = function() {
                 } else {
                     console.error("Unexpected response structure:", jsonResponse);
                 }
-        
+
                 console.log(todoList);
             }
         }
     };
-        
+
     req.open("GET", "https://api.jsonbin.io/v3/b/67142f0eacd3cb34a899d44e/latest", true);
     req.setRequestHeader("X-Master-Key", "$2a$10$AJpz2YeRbubCbtKW94i7lOvFvui8/y3lNRQbqMNnZaW6/fOlUA7FS");
     req.send();
 }
 
-let updateJSONbin = function() {
+let updateJSONbin = function () {
     // UWAGA: ta funkcja zastepuje całą zawartość bina
     let req = new XMLHttpRequest();
     req.open("PUT", "https://api.jsonbin.io/v3/b/67142f0eacd3cb34a899d44e", true);
@@ -83,9 +83,25 @@ let updateJSONbin = function() {
     req.send(JSON.stringify(todoList));
 }
 
-let deleteTodo = function(index) {
-    todoList.splice(index,1);
+let deleteTodo = function (index) {
+    todoList.splice(index, 1);
     updateJSONbin();
+}
+
+
+let dateFilter = function () {
+    let startDate = document.getElementById("inputStartDate").value ? new Date(document.getElementById("inputStartDate").value) : null;
+    let endDate = document.getElementById("inputEndDate").value ? new Date(document.getElementById("inputEndDate").value) : null;
+
+    return todoList.filter(todo => {
+
+        let dueDate = new Date(todo.dueDate); // Konwertowanie daty zadania na format Date
+
+        return (!startDate || dueDate >= startDate) &&
+            (!endDate || dueDate <= endDate);
+
+    })
+
 }
 
 
@@ -95,52 +111,26 @@ let updateTodoList = function () {
     while (todoListDiv.firstChild) {
         todoListDiv.removeChild(todoListDiv.firstChild);
     }
-    if (isTableView) {
-        renderTable(todoListDiv);
-    } else {
-        renderList(todoListDiv);
-    }
 
+
+    renderTable(todoListDiv);
 
 
 }
 
-let renderList = function(todoListDiv) {
-
-    //add all elements
-    let filterInput = document.getElementById("inputSearch");
-
-    //add all elements
-    for (let todo in todoList) {
-        if (
-            (filterInput.value == "") ||
-            (todoList[todo].title.includes(filterInput.value)) ||
-            (todoList[todo].description.includes(filterInput.value))
-        ) {
-            let newElement = document.createElement("p");
-            let newContent = document.createTextNode(todoList[todo].title + " " +
-                todoList[todo].description + " " + todoList[todo].place + " " + (todoList[todo].dueDate ? new Date(todoList[todo].dueDate).toLocaleDateString() : ""));
-            newElement.appendChild(newContent);
-            todoListDiv.appendChild(newElement);
-
-            let newDeleteButton = document.createElement("input");
-            newDeleteButton.type = "button";
-            newDeleteButton.value = "x";
-            newDeleteButton.addEventListener("click",
-                function() {
-                    deleteTodo(todo);
-                });
-
-            newElement.appendChild(newDeleteButton);
-        }
-    }
+let enableDateFilter = function () {
+    dateFilterEnabled = !dateFilterEnabled;
 
 }
 
 let renderTable = function (todoListDiv) {
 
+    let visibleList;
+
+    let filterInput = document.getElementById("inputSearch");
+
     let table = document.createElement("table");
-    table.classList.add("table","table-striped","table-hover","table-bordered");
+    table.classList.add("table", "table-striped", "table-bordered");
 
     let headerRow = document.createElement("tr");
     headerRow.classList.add("table-header");
@@ -151,91 +141,75 @@ let renderTable = function (todoListDiv) {
     });
     table.appendChild(headerRow);
 
-    todoList.forEach((todo, index) => {
-        let row = document.createElement("tr");
 
-        let titleCell = document.createElement("td");
-        titleCell.textContent = todo.title;
-        row.appendChild(titleCell);
+    if (dateFilterEnabled) {
+        visibleList = dateFilter();
+    } else {
+        visibleList = todoList
+    }
 
-        let descriptionCell = document.createElement("td");
-        descriptionCell.textContent = todo.description;
-        row.appendChild(descriptionCell);
 
-        let placeCell = document.createElement("td");
-        placeCell.textContent = todo.place;
-        row.appendChild(placeCell);
+    visibleList.forEach((todo, index) => {
 
-        let dateCell = document.createElement("td");
-        dateCell.textContent = todo.dueDate ? new Date(todo.dueDate).toLocaleDateString() : "";
-        row.appendChild(dateCell);
+            let textFilter = ((filterInput.value === "") ||
+                (todo.title.includes(filterInput.value)) ||
+                (todo.description.includes(filterInput.value)) ||
+                (todo.place.includes(filterInput.value)))
 
-        let deleteCell = document.createElement("td");
-        let deleteButton = document.createElement("button");
-        deleteButton.type = "button";
-        deleteButton.value = "x";
-        deleteButton.classList.add("btn", "btn-dark", "btn-sm");
-        deleteButton.addEventListener("click", function() {
-            deleteTodo(index);
-        });
-        deleteCell.appendChild(deleteButton);
-        row.appendChild(deleteCell);
+            if (
+                textFilter
+            ) {
 
-        table.appendChild(row);
-    });
+                let row = document.createElement("tr");
+
+                let titleCell = document.createElement("td");
+                titleCell.textContent = todo.title;
+                row.appendChild(titleCell);
+
+                let descriptionCell = document.createElement("td");
+                descriptionCell.textContent = todo.description;
+                row.appendChild(descriptionCell);
+
+                let placeCell = document.createElement("td");
+                placeCell.textContent = todo.place;
+                row.appendChild(placeCell);
+
+                let dateCell = document.createElement("td");
+                dateCell.textContent = todo.dueDate ? new Date(todo.dueDate).toLocaleDateString() : "";
+                row.appendChild(dateCell);
+
+                let deleteCell = document.createElement("td");
+                let deleteButton = document.createElement("button");
+                deleteButton.type = "button";
+                deleteButton.value = "x";
+                deleteButton.classList.add("btn", "btn-dark", "btn-sm");
+                deleteButton.addEventListener("click", function () {
+                    deleteTodo(index);
+                });
+                deleteCell.appendChild(deleteButton);
+                row.appendChild(deleteCell);
+
+                table.appendChild(row);
+            }
+        }
+    );
 
     todoListDiv.appendChild(table);
 
 };
-// let updateTodoList = function() {
-//     let todoListDiv =
-//     document.getElementById("todoListView");
-//
-//     //remove all elements
-//     while (todoListDiv.firstChild) {
-//         todoListDiv.removeChild(todoListDiv.firstChild);
-//     }
-//
-//     //add all elements
-//     let filterInput = document.getElementById("inputSearch");
-//
-//     //add all elements
-//     for (let todo in todoList) {
-//         if (
-//             (filterInput.value == "") ||
-//             (todoList[todo].title.includes(filterInput.value)) ||
-//             (todoList[todo].description.includes(filterInput.value))
-//         ) {
-//             let newElement = document.createElement("p");
-//             let newContent = document.createTextNode(todoList[todo].title + " " +
-//                                                      todoList[todo].description);
-//             newElement.appendChild(newContent);
-//             todoListDiv.appendChild(newElement);
-//
-//             let newDeleteButton = document.createElement("input");
-//             newDeleteButton.type = "button";
-//             newDeleteButton.value = "x";
-//             newDeleteButton.addEventListener("click",
-//                 function() {
-//                     deleteTodo(todo);
-//                 });
-//
-//             newElement.appendChild(newDeleteButton);
-//         }
-//     }
-// }
 
-let addTodo = function() {
+
+let addTodo = function () {
     //get the elements in the form
     let inputTitle = document.getElementById("inputTitle");
     let inputDescription = document.getElementById("inputDescription");
     let inputPlace = document.getElementById("inputPlace");
-    let inputDate = document.getElementById("inputDate");
+    let inputDueDate = document.getElementById("inputDueDate");
     //get the values from the form
     let newTitle = inputTitle.value;
     let newDescription = inputDescription.value;
     let newPlace = inputPlace.value;
-    let newDate = new Date(inputDate.value);
+    let newDate = new Date(inputDueDate.value);
     //create new item
     let newTodo = {
         title: newTitle,
@@ -250,13 +224,7 @@ let addTodo = function() {
     //window.localStorage.setItem("todos", JSON.stringify(todoList));
 
     updateJSONbin();
-  }
-
-let toggleView = function() {
-    isTableView = !isTableView;
-    updateJSONbin();
 }
-
 
 initList();
 
