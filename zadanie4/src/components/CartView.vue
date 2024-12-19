c14d76356ae6ec6c203175ed0c48c2c56a1e9e70
Piotr Janiszek <247678@edu.p.lodz.pl>
2024-12-20 00:59:09 +0100

    Implementacja wyświetlania koszyka

--- zadanie4/src/components/CartView.vue
+++ zadanie4/src/components/CartView.vue
@@ -4,6 +4,7 @@ import axios from "axios";
 export default {
   data() {
     return {
+      cart: []
     };
   },
   created() {
@@ -13,6 +14,13 @@ export default {
     async finalizeCart() {
       try {
         console.log("cart hello world");
+        const cart = JSON.parse(localStorage.getItem('cart')) || [];
+        for (const element of cart) {
+          console.log("id: "  + element.id + ", quantity: " + element.quantity)
+          const response = await axios.get(`http://localhost:3000/products/${element.id}`);
+          this.cart.push({id: element.id, quantity: element.quantity, product: response.data, price: response.data._price * element.quantity});
+        }
+        this.cart.forEach((element) => console.log(element))
       } catch (error) {
         console.error('Error loading cart: ', error);
       }
@@ -23,6 +31,29 @@ export default {
 
 <template>
   <h1>Chart View</h1>
+
+  <table class="table table-striped">
+    <thead>
+    <tr>
+      <th>Name</th>
+      <th>Price</th>
+      <th>Quantity</th>
+      <th></th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="product in this.cart" :key="cart.id">
+      <td>{{product.product._name}}</td>
+      <td>{{product.price}} PLN</td>
+      <td>{{product.quantity}}</td>
+      <td>
+        <label for="change-quantity" class="form-label">Change quantity</label>
+        <input id="change-quantity" type="number" class="form-control" v-model="product.quantity" min="1" />
+      </td>
+    </tr>
+    </tbody>
+  </table>
+
 </template>
 
 <style>
