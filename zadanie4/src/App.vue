9b51fe073b34a4b107b78b67f1ddd5417ceb7389
Łukasz <247660@edu.p.lodz.pl>
2025-01-19 23:10:12 +0100

    navbar

--- zadanie4/src/App.vue
+++ zadanie4/src/App.vue
@@ -12,13 +12,22 @@ export default {
   <div class="wrapper w-100 p-0 position-static">
     <header class="home-container w-100">
       <div class="container-fluid d-flex justify-content-between align-items-center px-0">
-        <img alt="Logo" class="logo" src="@/assets/logo.svg" style="height: 70%; max-height: 80px; margin-top: 1%; margin-left: 1%" />
-        <div class="text-center flex-grow-1 d-flex flex-column justify-content-center align-items-center" style="margin: auto; width: 100%; position: absolute">
+        <img
+            alt="Logo"
+            class="logo"
+            src="@/assets/logo.svg"
+            style="height: 70%; max-height: 80px; margin-top: 1%; margin-left: 1%;"
+        />
+        <div
+            class="text-center flex-grow-1 d-flex flex-column justify-content-center align-items-center"
+            style="margin: auto; width: 100%; position: absolute;"
+        >
           <h1 class="mb-0">Najmanka</h1>
           <p class="text-muted">Discover amazing products at unbeatable prices</p>
         </div>
         <nav class="navbar navbar-expand-lg navbar-light">
           <div class="container-fluid">
+
             <button
                 class="navbar-toggler"
                 type="button"
@@ -30,6 +39,7 @@ export default {
             >
               <span class="navbar-toggler-icon"></span>
             </button>
+
             <div class="collapse navbar-collapse" id="navbarNav">
               <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
@@ -42,11 +52,6 @@ export default {
                     <img src="@/assets/cart.svg" alt="Cart" height="45"/>
                   </RouterLink>
                 </li>
-                <!--<li class="nav-item">
-                  <RouterLink class="nav-link" to="/authentication">
-                    <img src="@/assets/login.png" alt="Log in" height="45"/>
-                  </RouterLink>
-                </li>-->
                 <li class="nav-item">
                   <RouterLink class="nav-link" to="/dashboard">
                     <img src="@/assets/dashboard.svg" alt="Dashboard" height="45"/>
@@ -60,24 +65,24 @@ export default {
     </header>
   </div>
 
-  <br />
-  <RouterView />
-  <br />
+  <br/>
+  <RouterView/>
+  <br/>
 
   <div class="features-container container my-5">
     <div class="row text-center">
       <div class="col-md-4">
-        <img src="@/assets/quality.png" alt="Quality" width="100" height="100" class="mb-3" />
+        <img src="@/assets/quality.png" alt="Quality" width="100" height="100" class="mb-3"/>
         <h5>Top Quality</h5>
         <p>We offer products of the highest quality to meet your needs.</p>
       </div>
       <div class="col-md-4">
-        <img src="@/assets/delivery.png" alt="Delivery" width="173" height="100" class="mb-3" />
+        <img src="@/assets/delivery.png" alt="Delivery" width="173" height="100" class="mb-3"/>
         <h5>Fast Delivery</h5>
         <p>Enjoy fast and reliable delivery for all your orders.</p>
       </div>
       <div class="col-md-4">
-        <img src="@/assets/support.png" alt="Support" width="100" height="100" class="mb-3" />
+        <img src="@/assets/support.png" alt="Support" width="100" height="100" class="mb-3"/>
         <h5>24/7 Support</h5>
         <p>Our team is available round the clock to assist you.</p>
       </div>
@@ -89,10 +94,11 @@ export default {
 body {
   background-color: #f8f9fa;
 }
+
 .home-container2 {
   background: url('@/assets/home-background.jpg') no-repeat center center;
   background-size: cover;
-  color: white;
+
   height: 125px;
   width: 100%;
   display: flex;
@@ -100,10 +106,12 @@ body {
   justify-content: center;
   text-align: center;
 }
+
 .home-container h1,
 .home-container p {
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
 }
+
 .features-container img {
   border-radius: 50%;
 }
@@ -113,27 +121,43 @@ body {
   height: 125px;
   --s: 50px;
   --c: #17cadd;
-  --_s: calc(2*var(--s)) calc(2*var(--s));
+  --_s: calc(2 * var(--s)) calc(2 * var(--s));
   --_g: 35.36% 35.36% at;
-  --_c: #0000 66%, #494adf 68% 70%,#0000 72%;
-  background:
-      radial-gradient(var(--_g) 100% 25%,var(--_c)) var(--s) var(--s)/var(--_s),
-      radial-gradient(var(--_g) 0 75%,var(--_c)) var(--s) var(--s)/var(--_s),
-      radial-gradient(var(--_g) 100% 25%,var(--_c)) 0 0/var(--_s),
-      radial-gradient(var(--_g) 0 75%,var(--_c)) 0 0/var(--_s),
-      repeating-conic-gradient(var(--c) 0 25%,#0000 0 50%) 0 0/var(--_s),
-      radial-gradient(var(--_c)) 0 calc(var(--s)/2)/var(--s) var(--s) var(--c);
+  --_c: #0000 66%, #494adf 68% 70%, #0000 72%;
+  background: radial-gradient(var(--_g) 100% 25%, var(--_c)) var(--s) var(--s)/var(--_s),
+  radial-gradient(var(--_g) 0 75%, var(--_c)) var(--s) var(--s)/var(--_s),
+  radial-gradient(var(--_g) 100% 25%, var(--_c)) 0 0/var(--_s),
+  radial-gradient(var(--_g) 0 75%, var(--_c)) 0 0/var(--_s),
+  repeating-conic-gradient(var(--c) 0 25%, #0000 0 50%) 0 0/var(--_s),
+  radial-gradient(var(--_c)) 0 calc(var(--s) / 2)/var(--s) var(--s) var(--c);
   background-attachment: fixed;
 }
 
 
 .navbar {
   position: sticky;
-
-  width: 10%;
+  top: 0;
+  z-index: 1030;
 }
 
+.navbar-toggler {
+  border: none;
+  outline: none;
+}
 
+.navbar-nav .nav-item .nav-link img {
+  background-color: transparent;
+  padding: 5px;
+  border-radius: 8px;
+}
+
+.navbar-collapse {
+
+  margin-left: 6px;
+  margin-top: 25px;
+  padding: 10px;
+  border-radius: 8px;
+}
 
 @media (max-width: 1000px) {
   .navbar-collapse {
@@ -141,11 +165,9 @@ body {
     top: 0;
     left: 0;
     right: 0;
-    background-color: #ffffff;
+
   }
 }
-
-
 
 
 </style>
