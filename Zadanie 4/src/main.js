60f508ad742227e5c1f71574dff4527047d7d93f
Piotr Janiszek <pietrasos567@gmail.com>
2025-02-07 20:30:57 +0100

    Introduction branch restructuration

--- Zadanie 4/src/main.js
+++
@@ -1,8 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from "@/router.js";
-
-createApp(App).use(router).mount('#app')
